d3bbb045f7cd5ae72bd4e39ba1283fcb49fb4a81
Diego Araujo <ferreiradearaujod@gmail.com>
2024-07-07 23:37:02 -0300

    feat: ajustando console

--- exercicios-01.js
+++ exercicios-01.js
@@ -1,15 +1,13 @@
 // Construa um algoritmo em JavaSript que determine (imprima) se um dado número N
 // inteiro (recebido através do teclado) é PAR ou ÍMPAR.
 
-
-
 // prompt retorna string/texto
 
 //parse === transformar
 var numero = parseInt(prompt("Digite um número inteiro: "))
 
 if (numero % 2 === 0) {
-    console.log(numero + "é PAR")
+    console.log(numero + " é PAR")
 } else {
-    console.log(numero + "é ÍMPAR")
+    console.log(numero + " é ÍMPAR")
 }
